9f3b85d88b7f1f4a4f641e228f8ec91fed68246b
SergeyKirintsev <kserg80@gmail.com>
2021-09-05 16:44:02 +0300

    format code

--- app.js
+++ app.js
@@ -23,16 +23,16 @@ app.use(express.static(__dirname + "/public"));
 app.use(browserDetectMiddleware);
 
 app.get('/logout', (req, res) => {
-    res.clearCookie('jwt').send('Куки удалены');
+  res.clearCookie('jwt').send('Куки удалены');
 });
 
 app.get('/login', (req, res) => {
 
-    const cookie = req.browser.name === 'safari' ? cookieOptionsSafari : cookieOptionsAll;
+  const cookie = req.browser.name === 'safari' ? cookieOptionsSafari : cookieOptionsAll;
 
-    res
-        .cookie('jwt', 'test-123', cookie)
-        .send('Готово')
+  res
+    .cookie('jwt', 'test-123', cookie)
+    .send('Готово')
 });
 
 app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
